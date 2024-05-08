3e1d8215b82ca43fdcd5917cadc237ecc19d1821
layali-khayat <145710621+layali-khayat@users.noreply.github.com>
2024-05-09 00:05:42 +0300

    Update Department.java

--- Department.java
+++ Department.java
@@ -1,39 +1,61 @@
 
-package com.mycompany.company.system;
 
+
+package com.mycompany.company.system;
 import java.util.ArrayList;
 
 
-public class Department implements Observer{
-    
-    public String name;
-    public Address address;
-    public Employee head;
-    ArrayList<Office> offices;
 
-    public Department(String name, Address address, Employee head) {
-        this.name = name;
-        this.address = address;
-        this.head = head;
-        this.offices = new ArrayList<>();
-    }
-    
-    public void addOffice(Office office) {
-        this.offices.add(office);
-    }
-    
-    public void removeOffice(Office office) {
-        this.offices.remove(office);
-    }
+    public class Department implements Observer{
 
-    @Override
-    public void update(Subject subject) {
         
-        if(subject instanceof Company) {
-            
-            Company company = (Company)subject;
-            
-            System.out.println("Company" + company.name + " was updated");
-        }
+      public String name;  
+      public Address address;   
+      public Employee head;
+       ArrayList<Office> offices;
+
+
+        
+    public Department(String name, Address address, Employee head) { 
+        public Department(String name, Address address, Employee head) {
+            this.name = name;
+            this.address = address;
+            this.head = head;
+            this.offices = new ArrayList<>();
+
+    } 
+
+        
+     public void addOffice(Office office) {
+
+
+         this.offices.add(office);
+
+
     }
+    
+
+        
+    public void removeOffice(Office office) {
+
+      this.offices.remove(office);
+
+
+     }
+
+
+            @Override
+
+        public void update(Subject subject) {
+         if(subject instanceof Company) {
+              Company company = (Company)subject;
+            System.out.println("Company" + company.name + " was updated");
+
+            
+            
+          
+
+       }
+   }
+    
 }
