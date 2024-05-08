3e39af9862af13d346eb86a08d7e21745ec9935e
layali-khayat <145710621+layali-khayat@users.noreply.github.com>
2024-05-08 23:41:37 +0300

    Update DefaultAddressFormatter.java

--- DefaultAddressFormatter.java
+++ DefaultAddressFormatter.java
@@ -2,12 +2,17 @@
 package com.mycompany.company.system;
 
 
+
 public class DefaultAddressFormatter implements AddressFormatter{
 
-    @Override
-    public String formatAddress(Address address) {
-       
-        return address.toString();
-    }
+     @Override
     
-}
+         public String formatAddress(Address address) {
+       
+          return address.toString();
+
+
+       }
+    
+
+ }
