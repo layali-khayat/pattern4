33691fbdd26189ab7f2cb40aa8fb86af35811d4f
SouadR0 <131975482+SouadR0@users.noreply.github.com>
2024-05-08 20:55:26 +0300

    Update CustomAddressFormatter.java

--- CustomAddressFormatter.java
+++ CustomAddressFormatter.java
@@ -3,10 +3,10 @@ package com.mycompany.company.system;
 
 
 public class CustomAddressFormatter implements AddressFormatter{
-    
     @Override
     public String formatAddress(Address address) {
         // Custom logic for formatting address
         return address.getStreetNo() + ", " + address.getCity() + ", " + address.getState();
     }
 }
+ 
